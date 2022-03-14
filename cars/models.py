09aa12af4bf29d9a210d45d30c20d3ca316bf2ab
Rick Rieger <rickrieger36251@gmail.com>
2022-03-14 16:17:29 -0400

    created car modle for cars app + registered cars and rest framework in project settings installed apps settings

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# car class is considered a django ora model
+# this is considered a code first approach.
+class Car(models.Model):
+  make = models.CharField(max_length=255)
+  model = models.CharField(max_length=255)
+  year = models.IntegerField()
+  price = models.DecimalField(max_digits=8, decimal_places=2)
